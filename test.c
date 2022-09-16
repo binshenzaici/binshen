e384b2b2ed97084a6d6d1f132bf2a7e7fed42f4e
binshenzaici <binshenzaici@outlook.com>
2022-09-16 23:50:10 +0800

    2350

--- test.c
+++ test.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 int main()
 {
+	double weight = 75.3;
 	int age = 20;
+	weight = weight - 10;
 	age = age + 1;
-	printf("%d", age);
+	printf("%lf\n", weight);
+	printf("%d\n", age);
 	return 0;
 }
