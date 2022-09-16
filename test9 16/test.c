d7174e84e50bee18919d5c4814a69534376fbb50
binshenzaici <binshenzaici@outlook.com>
2022-09-16 23:12:38 +0800

    ceshi

--- test9 16/test.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-	printf("hehe");
-	return 0;
-}
--- test9.16/test.c
+++
@@ -1,9 +0,0 @@
-int main()
-{
-	char ch = 'a';
-	int age = 20;
-	short num = 10;
-		float weight = 55.5;
-	double d = 0.0;
-	return 0;
-}
