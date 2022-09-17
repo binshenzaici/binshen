479fd9a8d1cb93040d2dd49e9ff9dfe3d504b6a9
binshenzaici <binshenzaici@outlook.com>
2022-09-17 08:49:57 +0800

    9.17代码1

---
+++ 9.17/9.17/test.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//int main()
+//{
+//	float height = 1.66;
+//	height = height + 0.05;
+//	printf("%f\n", height);
+//	double weight = 75.3;
+//	weight = 75.3 - 10;
+//	int age = 20;
+//	printf("%lf\n", weight);
+//	age = age + 1;
+//	printf("%d\n", age);
+//		return 0;
+//}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int sum = 0;
+	scanf("%d %d", &a,&b);
+	sum = a + b;
+	printf("sum=%d", sum);
+	return 0;
+}
