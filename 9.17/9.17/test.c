3aecf867a775045de6f24d117e0c625775d6d9b8
binshenzaici <binshenzaici@outlook.com>
2022-09-17 14:58:22 +0800

    917

--- 9.17/9.17/test.c
+++ 9.17/9.17/test.c
@@ -14,13 +14,72 @@
 //	printf("%d\n", age);
 //		return 0;
 //}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int sum = 0;
+//	scanf("%d %d", &a,&b);
+//	sum = a + b;
+//	printf("sum=%d", sum);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int sum = 0;
+//	sum = a + b;
+//	scanf("%d%d", &a, &b);
+//	printf("sum=%d", sum);
+//	return 0;
+//}
+#define max 10000
+//int main()
+//{
+//	int n = max;
+//	printf("n=%d\n", n);
+//	return 0;
+//}
+//enum sex
+//{
+//	male,
+//female,
+//secret
+//};
+//int main()
+//	{
+//	enum sex s = male;
+//	printf("%d\n", male);
+	//
+//int main()
+//{
+//	"abcdef";
+//	"hello bite";
+//	return 0;
+//}
+//int main()
+//{
+//	char arr[]="hello";
+//	return 0;
+//}
+//int main()
+//{
+//	char arr1[] = "abc";
+//	char arr2[] = { 'a','b','c','\0'};
+//	printf("%s\n", arr1);
+//	printf("%s\n", arr2);
+//	return 0;
+//
+//}
+#include<string.h>
 int main()
 {
-	int a = 0;
-	int b = 0;
-	int sum = 0;
-	scanf("%d %d", &a,&b);
-	sum = a + b;
-	printf("sum=%d", sum);
+	char arr1[] = "abc";
+	char arr2[] = { 'a','b','c',};
+	printf("%d\n", strlen(arr1));
+	printf("%d\n", strlen(arr2));
+	int len = strlen("abc");
+	printf("%d\n", len);
 	return 0;
 }
